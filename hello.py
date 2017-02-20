4cf11e06f6cd85cc5fedde8aaa332092eea1492a
LinaBroman <lina.broman@misu.su.se>
2017-02-20 10:31:14 +0100

    Added the runner.py and edited hello.py to include a function

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 def foo():
 	print('foo!')
-foo() # this is where I actually call the function
+foo() # this is where I actually call the functiongit help 
 
+def say_hello():
+	print("Hello World")
